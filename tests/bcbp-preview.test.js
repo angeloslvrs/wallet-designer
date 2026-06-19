51104219c4805e38f6c46e56059a16dc52c13b3c
Angelo Soliveres <angelosoliveres@wmail.com>
2026-06-20 00:11:48 +0800

    test(designer): use repo-standard happy-dom env for bcbp-preview (drop stray jsdom dep)

--- tests/bcbp-preview.test.js
+++ tests/bcbp-preview.test.js
@@ -1,4 +1,4 @@
-// @vitest-environment jsdom
+// @vitest-environment happy-dom
 import { describe, it, expect } from "vitest";
 import { showBcbpPreview } from "../apps/designer/src/bcbp-preview.js";
 
